310f9fb1ccd9bcba6a92454a87d32dea470c789f
Woodam Lim <woodam.lim@gmail.com>
2023-01-11 15:37:01 +0900

    Update 24.sql

--- ch09/24.sql
+++ ch09/24.sql
@@ -1,3 +1,5 @@
+-- Feature extraction
+
 ML.FEATURE_CROSS(STRUCT(
   IF(EXTRACT(dayofweek FROM start_date) BETWEEN 2 AND 6, 'weekday', 'weekend') AS dayofweek,
   ML.BUCKETIZE(EXTRACT(HOUR FROM start_date), [5, 10, 17]) AS hr
